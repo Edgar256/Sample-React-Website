e4fa002e5dd57321666e0d801f18aa0d9c2aa8f2
Edgar <edgar@getpoints.com>
2018-11-27 17:39:55 +0300

    added new updates to these files

--- src/App.js
+++ src/App.js
@@ -11,35 +11,293 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <div className="section-wrapper bg-maroon" id="startSection">
-            <div className="section-wrapper-inner">
-              <div className="logo-holder-side" />
-              <div className="logo-holder">
-                <img src={glLogo} alt="GeekLabel Logo" width="100%" />
-              </div>
-              <div className="logo-holder-side" />
+        <div className="section-wrapper bg-maroon" id="startSection">
+          <div className="section-wrapper-inner">
+            <div className="logo-holder-side" />
+            <div className="logo-holder">
+              <img src="images/gl-logo.png" alt="GeekLabel Logo" width="100%" />
             </div>
-            <div className="section-wrapper-inner">
-              <div className="slg-holder-side" />
-              <div className="slg-holder">
-                A team of self confessed geeks who are all about great digital
-                design
-              </div>
-              <div className="slg-holder-side" />
+            <div className="logo-holder-side" />
+          </div>
+          <div className="section-wrapper-inner">
+            <div className="slg-holder-side" />
+            <div className="slg-holder">
+              A team of self confessed geeks who are all about great digital
+              design
             </div>
-            <div className="section-wrapper-inner">
-              <div className="arrow-holder-sect" />
-              <div className="arrow-holder">
-                <a href="#sectionOne">
-                  {" "}
-                  <img src={btnRed} alt="btn-red" width="100%" />
-                </a>{" "}
+            <div className="slg-holder-side" />
+          </div>
+          <div className="section-wrapper-inner">
+            <div className="arrow-holder-sect" />
+            <div className="arrow-holder">
+              <a href="#sectionOne">
+                {" "}
+                <img src="images/btn-red.png" width="100%" />
+              </a>{" "}
+            </div>
+            <div className="arrow-holder-sect" />
+          </div>
+        </div>
+
+        <div className="section-wrapper" id="sectionOne">
+          <div className="section-wrapper-inner">
+            <div className="mockup-holder-side" />
+            <div className="mockup-holder">
+              <img src="images/mockups.png" alt="mockup image" width="100%" />{" "}
+            </div>
+            <div className="mockup-holder-side" />
+          </div>
+          <div className="section-wrapper-inner">
+            <div className="width-20" />
+            <div className="width-60 descr-txt">
+              Some agencies love <span className="txt-maroon">design</span>, but
+              don't know how to build.
+            </div>
+            <div className="width-20" />
+          </div>
+          <div className="section-wrapper-inner">
+            <div className="arrow-holder-sect" />
+            <div className="arrow-holder">
+              {" "}
+              <a href="#sectionTwo">
+                <img src="images/btn-red.png" width="100%" />
+              </a>{" "}
+            </div>
+            <div className="arrow-holder-sect" />
+          </div>
+        </div>
+
+        <div className="section-wrapper" id="sectionTwo">
+          <div className="section-wrapper-inner">
+            <div className="mockup-holder-side" />
+            <div className="mockup-holder">
+              <img src="images/build.png" alt="mockup image" width="100%" />{" "}
+            </div>
+            <div className="mockup-holder-side" />
+          </div>
+          <div className="section-wrapper-inner">
+            <div className="width-20" />
+            <div className="width-60" style="color: #3d3b3e;">
+              Some agencies know how to{" "}
+              <span className="txt-maroon">build</span> but can't do design
+            </div>
+            <div className="width-20" />
+          </div>
+          <div className="section-wrapper-inner">
+            <div className="arrow-holder-sect" />
+            <div className="arrow-holder">
+              <a href="#sectionThree">
+                {" "}
+                <img src="images/btn-red.png" width="100%" />
+              </a>{" "}
+            </div>
+            <div className="arrow-holder-sect" />
+          </div>
+        </div>
+
+        <div className="section-wrapper" id="sectionThree">
+          <div className="section-wrapper-inner">
+            <div className="mockup-holder-side" />
+            <div className="mockup-holder">
+              <img src="images/solution.png" alt="mockup image" width="100%" />{" "}
+            </div>
+            <div className="mockup-holder-side" />
+          </div>
+          <div className="section-wrapper-inner">
+            <div className="width-20" />
+            <div className="width-60" style="color: #3d3b3e;">
+              We love <span className="txt-maroon">both</span>
+            </div>
+            <div className="width-20" />
+          </div>
+          <div className="section-wrapper-inner">
+            <div className="arrow-holder-sect" />
+            <div className="arrow-holder">
+              <a href="#sectionFour">
+                {" "}
+                <img src="images/btn-red.png" width="100%" />
+              </a>{" "}
+            </div>
+            <div className="arrow-holder-sect" />
+          </div>
+        </div>
+
+        <div className="section-wrapper bg-white" id="sectionFour">
+          <div className="section-wrapper-inner">
+            <div className="header-side" />
+            <div className="header">Services </div>
+            <div className="header-side" />
+          </div>
+          <div className="ser-main">
+            <div className="width-10" />
+            <div className="ser-main-inner">
+              <div className="img-ser">
+                <img src="images/web-dev.png" width="50%" />
               </div>
-              <div className="arrow-holder-sect" />
+              <div className="ser-txt">Development</div>
+            </div>
+            <div className="ser-main-inner">
+              <div className="img-ser">
+                <img src="images/design.png" width="50%" />
+              </div>
+              <div className="ser-txt">Design</div>
+            </div>
+            <div className="ser-main-inner">
+              <div className="img-ser">
+                <img src="images/branding.png" width="50%" />
+              </div>
+              <div className="ser-txt">Branding</div>
+            </div>
+            <div className="ser-main-inner">
+              <div className="img-ser">
+                <img src="images/ux-research.png" width="50%" />
+              </div>
+              <div className="ser-txt">UX Reseach</div>
+            </div>
+            <div className="width-10" />
+          </div>
+          <div className="section-wrapper-inner">
+            <div className="arrow-holder-sect" />
+            <div className="arrow-holder">
+              <a href="#sectionFive">
+                {" "}
+                <img src="images/ghost-btn-black.png" width="100%" />
+              </a>{" "}
+            </div>
+            <div className="arrow-holder-sect" />
+          </div>
+        </div>
+
+        <div className="section-wrapper" id="sectionFive">
+          <div className="section-wrapper-inner">
+            <div className="header-side" />
+            <div className="header">Clients </div>
+            <div className="header-side" />
+          </div>
+          <div className="ptnr-sctn">
+            <div className="width-12">
+              <img src="images/chevron-left.png" width="10%" />
+            </div>
+            <div className="width-25">
+              <img src="images/logo-tpg.png" width="50%" />{" "}
+            </div>
+            <div className="width-25">
+              <img src="images/logo-tvs.png" width="50%" />
+            </div>
+            <div className="width-25">
+              <img src="images/logo-pshe.png" width="50%" />
+            </div>
+            <div className="width-12">
+              <img src="images/chevron-right.png" width="10%" />
             </div>
           </div>
-        </header>
+          <div className="section-wrapper-inner">
+            <div className="arrow-holder-sect" />
+            <div className="arrow-holder">
+              <a href="#sectionSix">
+                {" "}
+                <img src="images/ghost-btn-black.png" width="100%" />
+              </a>{" "}
+            </div>
+            <div className="arrow-holder-sect" />
+          </div>
+        </div>
+
+        <div className="section-wrapper" id="sectionSix">
+          <div className="section-wrapper-inner">
+            <div className="header-side" />
+            <div className="header">How to find us </div>
+            <div className="header-side" />
+          </div>
+          <div className="container-map">
+            <div id="map" />
+            <div className="map-pin">
+              <img src="images/map-pin.png" alt="map-pin.png" width="100%" />
+            </div>
+          </div>
+          <div className="section-wrapper-inner">
+            <div className="arrow-holder-sect" />
+            <div className="arrow-holder">
+              <a href="#sectionSeven">
+                {" "}
+                <img src="images/btn-red.png" width="100%" />
+              </a>{" "}
+            </div>
+            <div className="arrow-holder-sect" />
+          </div>
+        </div>
+
+        <div
+          className="section-wrapper"
+          style="/*margin: 0px 0px -30px 0px;*/"
+          id="sectionSeven"
+        >
+          <div className="section-wrapper-inner">
+            <div className="header-side" />
+            <div className="header">Contact </div>
+            <div className="header-side" />
+          </div>
+
+          <div className="container-contact">
+            <form>
+              <div className="input-wrapper">
+                <div className="input-holder-txt-side" />
+                <div className="input-holder-txt">
+                  <input type="text" name="name" placeholder="Name" />{" "}
+                </div>
+                <div className="input-holder-txt-side" />
+              </div>
+
+              <div className="input-wrapper">
+                <div className="input-holder-txt-side" />
+                <div className="input-holder-txt">
+                  <input type="email" name="name" placeholder="Email" />{" "}
+                </div>
+                <div className="input-holder-txt-side" />
+              </div>
+
+              <div className="input-wrapper">
+                <div className="input-holder-txt-side" />
+                <div className="input-holder-txt">
+                  <textarea
+                    type="email"
+                    name="message"
+                    id="message"
+                    placeholder="Message"
+                    style="height:200px"
+                  >
+                    {" "}
+                  </textarea>
+                </div>
+                <div className="input-holder-txt-side" />
+              </div>
+
+              <div className="input-wrapper">
+                <div className="input-holder-txt-side" />
+                <div className="input-holder-txt">
+                  <button>Send Message</button>
+                </div>
+                <div className="input-holder-txt-side" />
+              </div>
+
+              <div className="input-wrapper">
+                <div className="input-holder-txt-side" />
+                <div className="input-holder-txt footer-txt">
+                  Or phone on: 01923 220121
+                </div>
+                <div className="input-holder-txt-side" />
+              </div>
+
+              <div className="input-wrapper">
+                <div className="input-holder-txt-side" />
+                <div className="input-holder-txt footer-txt">
+                  Or Email at info@compucorp.co.uk
+                </div>
+                <div className="input-holder-txt-side" />
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
     );
   }
